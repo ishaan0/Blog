29376c3d4495f16ef62a5b8c2d20705214c0ed8e
Muhammad Ishaan <iftekharctgbd@gmail.com>
2024-03-25 17:21:59 +0600

    refactor: add configure service extension methods

--- Blog.Api/Program.cs
+++ Blog.Api/Program.cs
@@ -1,71 +1,8 @@
-using Blog.Application.ServiceContracts;
-using Blog.Application.Services;
-using Blog.Domain.IdentityEntities;
-using Blog.Infrastructure.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using Blog.Api.StartupExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddTransient<IJwtService, JwtService>();
-
-builder.Services.AddControllers(options =>
-{
-    //Authorization policy
-    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-    options.Filters.Add(new AuthorizeFilter(policy));
-});
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ApplicationDbContext>(
-    (options) =>
-    {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-    });
-
-builder.Services.AddIdentity<User, Role>
-    (options =>
-    {
-        options.Password.RequiredLength = 5;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireDigit = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequiredUniqueChars = 3;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders()
-    .AddUserStore<UserStore<User, Role, ApplicationDbContext, Guid>>()
-    .AddRoleStore<RoleStore<Role, ApplicationDbContext, Guid>>();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
-builder.Services.AddAuthorization(options => { });
+builder.Services.AddConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Blog.Api/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,102 @@
+﻿using Blog.Application.ServiceContracts;
+using Blog.Application.Services;
+using Blog.Domain.IdentityEntities;
+using Blog.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blog.Api.StartupExtensions
+{
+    public static class ConfigureServicesExtension
+    {
+        public static IServiceCollection AddConfigureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddControllers(options =>
+            {
+                //Authorization policy
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddServices();
+            services.AddSwagger();
+
+            // Database connection
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddAuthentication(configuration);
+
+            services.AddAuthorization(options => { });
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IJwtService, JwtService>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+        private static IServiceCollection AddAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            // Identity configuration
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.Password.RequiredLength = 5;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 3;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders()
+            .AddUserStore<UserStore<User, Role, ApplicationDbContext, Guid>>()
+            .AddRoleStore<RoleStore<Role, ApplicationDbContext, Guid>>();
+
+            // Authentication configuration with jwt
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = true,
+                    ValidAudience = configuration["Jwt:Audience"],
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        System.Text.Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                };
+            });
+
+            return services;
+        }
+    }
+}
