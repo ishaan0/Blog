4edd4d038a17a4a69cbbc9e220bd792c8febee12
Muhammad Ishaan <iftekharctgbd@gmail.com>
2024-04-14 22:00:25 +0600

    feat: add user repository partially

---
+++ Blog.Domain/Interfaces/Persistence/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Domain.IdentityEntities;
+using Blog.Domain.Models;
+
+namespace Blog.Domain.Interfaces.Persistence.Repositories;
+
+public interface IUserRepository
+{
+    Task<PaginatedList<User>> GetAllAsync(
+        PaginationQuery<User> query, CancellationToken cancellationToken = default);
+    Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task CreateAsync(User user, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+}
+
---
+++ Blog.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using Blog.Domain.Exceptions;
+using Blog.Domain.IdentityEntities;
+using Blog.Domain.Interfaces.Persistence.Repositories;
+using Blog.Domain.Messages;
+using Blog.Domain.Models;
+using Blog.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure.Repositories;
+
+internal class UserRepository(ApplicationDbContext context) : IUserRepository
+{
+    public async Task CreateAsync(User user, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(user);
+        await context.Users.AddAsync(user);
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        if (!await context.Users.AnyAsync(u => u.Id == id, cancellationToken))
+        {
+            throw new NotFoundException(UserMessages.NotFound);
+        }
+        var user = context.Users.FirstOrDefault(u => u.Id == id);
+        context.Remove(user);
+    }
+
+    public async Task<PaginatedList<User>> GetAllAsync(PaginationQuery<User> query, CancellationToken cancellationToken = default)
+    {
+        var queryble = context.Users
+            .Where(query.Filter)
+            .OrderBy(u => u.Name);
+
+        var users = await queryble
+            .Skip(query.PageSize * (query.PageNumber - 1))
+            .Take(query.PageSize)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return new PaginatedList<User>(
+            users,
+            new PaginationMetadata(
+                await queryble.CountAsync(),
+                query.PageNumber,
+                query.PageSize)
+            );
+    }
+
+    public Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(User user, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
