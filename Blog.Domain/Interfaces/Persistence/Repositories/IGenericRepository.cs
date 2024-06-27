fa5686cf0a0f669f33630fb10ed120b415cd2f8f
Muhammad Ishaan <iftekharctgbd@gmail.com>
2024-06-27 21:54:11 +0600

    feat(server): add generic repository

---
+++ Blog.Domain/Interfaces/Persistence/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Blog.Domain.Interfaces.Persistence.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> GetByConditionAsync(
+        Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task CreateAsync(T entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    Task SaveAsync(CancellationToken cancellationToken = default);
+}
---
+++ Blog.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using Blog.Domain.Interfaces.Persistence.Repositories;
+using Blog.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Blog.Infrastructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbSet
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<T>> GetByConditionAsync(
+        Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default)
+    {
+        return await _dbSet
+                .AsNoTracking()
+                .Where(expression)
+                .ToListAsync(cancellationToken);
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.FindAsync(new object[] { id }, cancellationToken);
+    }
+
+    public async Task CreateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+        await _dbSet.AddAsync(entity, cancellationToken);
+    }
+
+    public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+        _dbSet.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+        _dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveAsync(CancellationToken cancellationToken = default)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
